11bd708793af45a8b5d5e876c9288165ef58975b
Gijs Kant <gijs@thehyve.nl>
2016-02-18 16:02:00 +0100

    Prevent test fail because of missing log service.
    In testing (or other situations where logging services
    are not injected), this patch prevents the
    controllers from failing.

--- grails-app/conf/org/transmartproject/rest/AuditLogFilters.groovy
+++ grails-app/conf/org/transmartproject/rest/AuditLogFilters.groovy
@@ -15,11 +15,11 @@ class AuditLogFilters {
                 def fullUrl = "${request.forwardURI}${request.queryString ? '?' + request.queryString : ''}"
                 def ip = request.getHeader('X-FORWARDED-FOR') ?: request.remoteAddr
 
-                accessLogService.report(currentUserBean, 'REST API Data Retrieval',
+                accessLogService?.report(currentUserBean, 'REST API Data Retrieval',
                         eventMessage:  "User (IP: ${ip}) got low dim. data with ${fullUrl}",
                         requestURL: fullUrl)
 
-                auditLogService.report("REST API access (low dim)", request,
+                auditLogService?.report("REST API access (low dim)", request,
                         user: currentUserBean,
                         action: fullUrl as String
                 )
@@ -31,11 +31,11 @@ class AuditLogFilters {
                 def fullUrl = "${request.forwardURI}${request.queryString ? '?' + request.queryString : ''}"
                 def ip = request.getHeader('X-FORWARDED-FOR') ?: request.remoteAddr
 
-                accessLogService.report(currentUserBean, 'REST API Data Retrieval',
+                accessLogService?.report(currentUserBean, 'REST API Data Retrieval',
                         eventMessage:  "User (IP: ${ip}) got high dim. data with ${fullUrl}",
                         requestURL: fullUrl)
 
-                auditLogService.report("REST API access (high dim)", request,
+                auditLogService?.report("REST API access (high dim)", request,
                         user: currentUserBean,
                         action: fullUrl as String
                 )
@@ -44,7 +44,7 @@ class AuditLogFilters {
         resources(controller: 'study|concept|subject|patientSet', action: '*') {
             before = { model ->
                 def fullUrl = "${request.forwardURI}${request.queryString ? '?' + request.queryString : ''}"
-                auditLogService.report("REST API access (${controllerName}.${actionName})", request,
+                auditLogService?.report("REST API access (${controllerName}.${actionName})", request,
                         user: currentUserBean,
                         action: fullUrl as String
                 )
